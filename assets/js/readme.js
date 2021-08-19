50e04ee4ba604c390f22a9982d4070335c378e21
mrralphpierre <mrralphpierre@gmail.com>
2021-08-19 17:58:45 -0400

    added fs, inquire, and question obj

---
+++ assets/js/readme.js
@@ -0,0 +1,40 @@
+var fileGenerator = require("./filegenerator");
+    var fs = require("fs");
+    var inquire = require("inquirer");
+
+// Questions object that will be looped through to ask the user //
+const questions = [
+    {
+        type: 'input'
+        name: 'title'
+        message: `What is the title of your project?`,
+    },
+    {
+        type: 'input'
+        name: 'title'
+        message: `What is the title of your project?`,
+    },
+    {
+        type: 'input'
+        name: 'title'
+        message: `What is the title of your project?`,
+    },
+    {
+        type: 'input'
+        name: 'title'
+        message: `What is the title of your project?`,
+    },
+    {
+        type: 'input'
+        name: 'title'
+        message: `What is the title of your project?`,
+    },  {
+        type: 'input'
+        name: 'title'
+        message: `What is the title of your project?`,
+    },  {
+        type: 'input'
+        name: 'title'
+        message: `What is the title of your project?`,
+    },
+]
